f36a7ff8ad46578ad1950ef6991440b84f76c524
Aydin Maleki <2148169@talnet.nl>
2022-03-11 13:06:45 +0100

    opd2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES ("Frans", "Schaap", "9C", "1999-05-16", "Amsterdam")
 
+INSERT INTO studenten (voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES ("Jaden", "Westwijk", "9C", "2000-08-12", "Aalsmeer")
+
+INSERT INTO studenten (voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES ("Max", "Bischop", "9C", "2000-05-10", "Aalsmeer")
