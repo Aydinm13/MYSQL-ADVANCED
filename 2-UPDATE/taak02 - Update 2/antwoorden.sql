7dc890a9a65e9a0106ab2a387d23af1ef816b848
Aydin Maleki <2148169@talnet.nl>
2022-03-14 09:06:15 +0100

    opd4

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,19 @@
 -- Opdracht 1
+UPDATE studenten2
+set klas = "9X"
+WHERE klas = "9A"
 
 -- Opdracht 2 
+UPDATE studenten2
+set klas = "9Z"
+WHERE student_id > 7
 
 -- Opdracht 3
+UPDATE studenten2
+set woonplaats = "Amsterdam"
+WHERE student_id <= 2 OR student_id > 7
 
 -- Opdracht 4
-
+UPDATE studenten2
+set woonplaats = "Uithoorn"
+WHERE woonplaats = "Amsterdam" AND klas = "9X"
